f2be0bdf49e5505bdd059dee08f4125291b0ee2d
Andrew Magdy <89481184+Andrew-Magdy-Rizk@users.noreply.github.com>
2023-10-01 21:49:02 +0300

    new create gllary

--- main.js
+++ main.js
@@ -1,50 +1,66 @@
 //Buttom right
-let clickRight = document.getElementById("right");
-clickRight.addEventListener("click", function () {
-  let myBullets = Array.from(document.querySelectorAll(".landing .bullets li"));
-  let backGround = document.getElementById("landing");
-  for (let i = 0; i < myBullets.length; i++) {
-    if (myBullets[i] === myBullets[2]) {
-      if (myBullets[i].className === "active") {
-        myBullets[i].removeAttribute("class");
-        myBullets[0].setAttribute("class", "active");
-        backGround.style.cssText = "background-image: url(Image/landing3.jpg);";
-      }
-    }
 
-    if (myBullets[i].className === "active") {
+let clickRight = document.getElementById("right");
+let myBullets = document.querySelectorAll(".landing .bullets li");
+let backGround = document.getElementById("landing");
+let clickLeft = document.getElementById("left");
+
+clickRight.addEventListener("click", () => {
+  for (let i = 0; i < myBullets.length; i++) {
+    if (myBullets[i] === myBullets[2] && myBullets[i].className === "active") {
+      myBullets[i].removeAttribute("class");
+      myBullets[0].setAttribute("class", "active");
+      backGround.style.cssText = "background-image: url(Image/landing-1.jpg);";
+    } else if (myBullets[i].className === "active") {
       myBullets[i].removeAttribute("class");
       myBullets[++i].setAttribute("class", "active");
       if (myBullets[i] === myBullets[1]) {
-        backGround.style.cssText = "background-image: url(Image/landing.jpg);";
-      } else if (myBullets[i] === myBullets[2]) {
-        backGround.style.cssText = "background-image: url(Image/landing4.jpg);";
+        backGround.style.cssText =
+          "background-image: url(Image/landing-2.jpg);";
+      } else {
+        backGround.style.cssText =
+          "background-image: url(Image/landing-3.jpg);";
       }
     }
   }
 });
-//Buttom Left
-let clickLeft = document.getElementById("left");
-clickLeft.addEventListener("click", function () {
-  let myBullets = Array.from(document.querySelectorAll(".landing .bullets li"));
-  let backGround = document.getElementById("landing");
-  for (let i = myBullets.length - 1; i >= 0; i--) {
-    if (myBullets[i] === myBullets[0]) {
-      if (myBullets[i].className === "active") {
-        myBullets[i].removeAttribute("class");
-        myBullets[2].setAttribute("class", "active");
-        backGround.style.cssText = "background-image: url(Image/landing4.jpg);";
-      }
-    }
 
-    if (myBullets[i].className === "active") {
+//Buttom Left
+
+clickLeft.addEventListener("click", () => {
+  for (let i = myBullets.length - 1; i >= 0; i--) {
+    if (myBullets[i] === myBullets[0] && myBullets[i].className === "active") {
+      myBullets[i].removeAttribute("class");
+      myBullets[myBullets.length - 1].setAttribute("class", "active");
+      backGround.style.cssText = "background-image: url(Image/landing-3.jpg);";
+    } else if (myBullets[i].className === "active") {
       myBullets[i].removeAttribute("class");
       myBullets[--i].setAttribute("class", "active");
       if (myBullets[i] === myBullets[1]) {
-        backGround.style.cssText = "background-image: url(Image/landing.jpg);";
-      } else if (myBullets[i] === myBullets[0]) {
-        backGround.style.cssText = "background-image: url(Image/landing3.jpg);";
+        backGround.style.cssText =
+          "background-image: url(Image/landing-2.jpg);";
+      } else {
+        backGround.style.cssText =
+          "background-image: url(Image/landing-1.jpg);";
       }
     }
   }
 });
+
+// Click Direct on Bullets
+
+for (let i = 0; i < myBullets.length; i++) {
+  myBullets[i].addEventListener("click", () => {
+    for (let j = 0; j < myBullets.length; j++) {
+      myBullets[j].removeAttribute("class");
+    }
+    myBullets[i].setAttribute("class", "active");
+    if (myBullets[i] === myBullets[0]) {
+      backGround.style.cssText = "background-image: url(Image/landing-1.jpg);";
+    } else if (myBullets[i] === myBullets[1]) {
+      backGround.style.cssText = "background-image: url(Image/landing-2.jpg);";
+    } else {
+      backGround.style.cssText = "background-image: url(Image/landing-3.jpg);";
+    }
+  });
+}
